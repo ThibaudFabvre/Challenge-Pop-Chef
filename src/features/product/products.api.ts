f9512f47ff36f9fc78a4fb1fcaf0f294f1b0ef16
ThibaudFabvre <thibaudfabvre@gmail.com>
2022-04-07 10:59:15 +0200

    reset project without broken server

---
+++ src/features/product/products.api.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+const BASE_URL = 'http://localhost:3001/';
+// A mock function to mimic making an async request for data
+// export function fetchProduct({ id: 0, }) {
+//   axios.get('products/' + id);
+// }
