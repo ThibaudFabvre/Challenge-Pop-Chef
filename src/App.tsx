a5aaeb7cfc54a495e7048c6f671be838584043b3
ThibaudFabvre <thibaudfabvre@gmail.com>
2022-04-07 11:42:12 +0200

    feat: PC-01 add fetch all products with mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { fetchAllProducts, selectProducts } from './features/product/productSlice';
+import { useEffect } from 'react';
+
+
 
 function App() {
+  const dispatch = useAppDispatch();
+  const { list } = useAppSelector(selectProducts);
+
+  useEffect(() => {
+    dispatch(fetchAllProducts());
+  }, []);
+
   return (
     <div className="App">
+      {list.map(({name, id, description}, index) => (
+        <div key={index+id}>
+          <p key={name+id}>{name}</p>
+          <p key={index+name+id}>{description}</p>
+        </div>
+      ))}
     </div>
   );
 }
--- src/features/product/products.api.ts
+++ src/features/product/products.api.ts
@@ -1,6 +1,19 @@
 import axios from "axios";
 const BASE_URL = 'http://localhost:3001/';
+
+const testData = { 
+  productsList: [
+    { name: 'patate', description: 'blablablabla', id: 1},
+    { name: 'tomate', description: 'blablablabla', id: 2}, 
+    { name: 'carotte', description: 'blablablabla', id: 30},
+    { name: 'pomme', description: 'blablablabla', id: 4},
+    { name: 'poire', description: 'blablablabla', id: 500},  
+  ],
+};
 // A mock function to mimic making an async request for data
-// export function fetchProduct({ id: 0, }) {
-//   axios.get('products/' + id);
-// }
+export function fetchProducts() {
+  //  axios.get('products/' + id);
+  setTimeout(()=> { 
+  }, 1000);
+  return testData;
+}
